24d5e5467a62b688f6de17ae3d0d3ba91c369dd5
Alexander Ippatiev <33111889+alexanderippatiev@users.noreply.github.com>
2020-06-01 12:37:24 +0900

    new updates

---
+++ layouts/default.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app style="background-color: #A8C7AB">
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    />
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
+
+<style>
+
+  body {
+    font-family: 'Source Code Pro', monospace;
+  }
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-layout>
+    <nuxt-link  style="font-family: 'Source Code Pro', monospace;" to="/projects"><h1>Projects</h1></nuxt-link>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+
+<style scoped>
+
+</style>
